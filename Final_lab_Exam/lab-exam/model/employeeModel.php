39c63a750e66300d8877f496c3c431f24ea04ac4
fardin705 <158167111+fardin705@users.noreply.github.com>
2025-01-12 16:29:41 +0600

    Add files via upload

---
+++ Final_lab_Exam/lab-exam/model/employeeModel.php
@@ -0,0 +1,100 @@
+<?php
+require_once('db.php');
+
+function addEmployee($name, $company, $contact, $username, $password)
+{
+    $con = dbConnection();
+    $sql = "INSERT INTO employee (name, company, contact, username, password) 
+            VALUES ('{$name}', '{$company}', '{$contact}', '{$username}', '{$password}')";
+
+    if (mysqli_query($con, $sql)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isExistUser($username)
+{
+    $con = dbConnection();
+    $sql = "SELECT * FROM employee WHERE username='{$username}'";
+    $result = mysqli_query($con, $sql);
+
+    return mysqli_num_rows($result) > 0;
+}
+
+function loginUser($username, $password)
+{
+    $con = dbConnection();
+
+    $query = "SELECT * FROM employee WHERE username = ? LIMIT 1";
+    $stmt = $con->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+
+        if ($user['password'] === $password) {
+            return $user;
+        }
+    }
+
+    return false;
+}
+
+function getAllEmployees()
+{
+    $conn = dbConnection();
+
+    $query = "SELECT * FROM employee";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $employees = [];
+        while ($row = $result->fetch_assoc()) {
+            $employees[] = $row;
+        }
+        return $employees;
+    }
+    return [];
+}
+
+function deleteEmployee($id)
+{
+    $conn = dbConnection();
+
+    $query = "DELETE FROM employee WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
+
+    return $stmt->execute();
+}
+
+
+function updateEmployee($id, $name, $company, $contact, $username)
+{
+    $conn = dbConnection();
+
+    $query = "UPDATE employee SET name = ?, company = ?, contact = ?, username = ? WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ssssi", $name, $company, $contact, $username, $id);
+
+    return $stmt->execute();
+}
+
+function getEmployeeById($id)
+{
+    $conn = dbConnection();
+
+    $query = "SELECT * FROM employee WHERE id = ? LIMIT 1";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    return $result->fetch_assoc();
+}
+
+?>
---
+++ Final_lab_Exam/lab_exam.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2025 at 11:05 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lab_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `company` varchar(100) NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `name`, `company`, `contact`, `username`, `password`) VALUES
+(1, 'Sajib', 'dcl', '01111111111', 'sajib', 'fardin'),
+(2, 'fardin', 'fardin', '11111111111', 'fardin', 'fardinfardin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
